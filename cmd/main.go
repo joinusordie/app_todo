cb844755da345f9251e0ad36215d4013e552571b
joinusordie <vengrisha2015@gmail.com>
2023-02-22 13:27:03 +0300

    Dependency Injection

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,27 @@ import (
 
 	todo "github.com/joinusordie/app_todo"
 	"github.com/joinusordie/app_todo/pkg/handler"
+	"github.com/joinusordie/app_todo/pkg/repository"
+	"github.com/joinusordie/app_todo/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+	
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
