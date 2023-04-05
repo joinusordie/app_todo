5add70b9bde578a278237f3de7795680fced747f
joinusordie <vengrisha2015@gmail.com>
2023-04-05 15:50:15 +0300

    GetAll for items

--- pkg/repository/todo_item_postgres.go
+++ pkg/repository/todo_item_postgres.go
@@ -15,7 +15,7 @@ func NewTodoItemPostgres(db *sqlx.DB) *TodoItemPostgres {
 	return &TodoItemPostgres{db: db}
 }
 
-func (r *TodoItemPostgres)  Create( listId int, item todo.TodoItem) (int, error) {
+func (r *TodoItemPostgres)  Create(listId int, item todo.TodoItem) (int, error) {
 	tx, err := r.db.Begin()
 	if err != nil {
 		return 0, err
@@ -31,13 +31,25 @@ func (r *TodoItemPostgres)  Create( listId int, item todo.TodoItem) (int, error)
 		return 0, err
 	}
 
-	createListItemQuery := fmt.Sprintf("INSERT INTO %s (list_id, item_id) values ($1, $2)", todoItemsTable)
+	createListItemsQuery := fmt.Sprintf("INSERT INTO %s (list_id, item_id) values ($1, $2)", listsItemsTable)
 
-	_, err = tx.Exec(createListItemQuery, listId, itemId)
+	_, err = tx.Exec(createListItemsQuery, listId, itemId)
 	if err != nil {
 		tx.Rollback()
 		return 0, err
 	}
 
 	return itemId, tx.Commit()
+}
+
+func (r *TodoItemPostgres) GetAll(userId,listId int) ([]todo.TodoItem, error) {
+	var items []todo.TodoItem
+	query := fmt.Sprintf(`SELECT ti.id, ti.title, ti.description, ti.done FROM %s ti INNER JOIN %s li on li.item_id = ti.id
+							INNER JOIN %s ul on ul.list_id = li.list_id WHERE li.list_id = $1 AND ul.user_id = $2`,
+						todoItemsTable, listsItemsTable, usersListsTable)
+	if err := r.db.Select(&items, query, listId, userId); err != nil {
+		return nil, err
+	}
+
+	return items, nil
 }
